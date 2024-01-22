475a1d4b7500ed27d2e02d3801fa3ab0e2874bf6
Saladuit <saladuit@gmail.com>
2024-01-22 19:52:52 +0100

    ?

--- lua/plugins/coc.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "neoclide/coc.nvim",
-  },
-}
--- Need to cd into ~/.data/nvim/lazy/coc.nvim/ && git checkout release
--- Still need to run :CocInstall @yaegassy/coc-volar
--- Still need to run :CocInstall @yaegassy/coc-volar-tools
